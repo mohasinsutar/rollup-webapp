83a3a0830a50da009d930d660715a42e68ec5aba
mohasin <mohasin@cdac.in>
2019-04-01 23:21:25 +0530

    imported external library faker to main.js. Added http-server for live reloading during development

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
 // rollup.config.js
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
 
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
 export default {
   input: 'src/main.js',
   output: {
@@ -11,9 +16,12 @@ export default {
     sourcemap: true,
   },
   plugins: [
+    //commonjs plugin should go first to identify commonjs nature of the imported module.
+    commonjs(),
     resolve(),
     babel({
       exclude: 'node_modules/**' // only transpile our source code
-    })
+    }),
+    production && terser() // minify in production
   ]
 };
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
+import faker from 'faker';
 var Main = {
     sayHello: function(){
         console.log('i am here to say hello');
     },
     sayHi: function(){
       console.log('I am here to say Hi');
+    },
+    printDemo: function(){
+        return (
+            faker.name.findName() + '<br/>' + faker.internet.email() + '<br/>' + faker.address.city() + '<br/>' + faker.address.zipCode()
+        );
     }
 };
 export default Main;
